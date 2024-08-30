feab7e6776b8c6fdb7c0d632a5eaaa0eb13727c0
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-30 15:34:50 +0530

    added modules from other script files

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import { pageLoad } from "./home.js";
+import { menuLoad } from "./menu.js";
+import { contactLoad } from "./contact.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  pageLoad();
+});
 
 const homeBtn = document.querySelector(".home");
 homeBtn.addEventListener("click", () => {
   pageLoad();
 });
 
+const menuBtn = document.querySelector(".menu");
+menuBtn.addEventListener("click", () => {
+  menuLoad();
+});
 
+const contactBtn = document.querySelector(".contact");
+contactBtn.addEventListener("click", () => {
+  contactLoad();
+});

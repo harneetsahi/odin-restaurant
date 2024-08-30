a146e47d20aaff192b6e0e64cfef6c9859583145
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-30 15:36:04 +0530

    added classes for css and organized the code

--- home.js
+++ home.js
@@ -3,18 +3,22 @@ export function pageLoad() {
 
   container.innerHTML = "";
 
-  let headHTML = document.createElement("h2");
+  const homeContent = document.createElement("div");
+  homeContent.classList.add("home-content");
+
+  const headHTML = document.createElement("h2");
   headHTML.innerHTML = "Breakfast for morning kick";
 
-  let paraHTML = document.createElement("p");
+  const paraHTML = document.createElement("p");
 
   paraHTML.innerHTML =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 
-  let contentImage = document.createElement("img");
+  const contentImage = document.createElement("img");
   contentImage.src = "./food.jpg";
 
-  container.appendChild(contentImage);
-  container.appendChild(headHTML);
-  container.appendChild(paraHTML);
+  homeContent.appendChild(headHTML);
+  homeContent.appendChild(contentImage);
+  homeContent.appendChild(paraHTML);
+  container.appendChild(homeContent);
 }
